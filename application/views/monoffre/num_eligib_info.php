380a84e4348c5ac5c7bf197703b1191ad7e897e5
swanand9 <swanand.reddy@gmail.com>
2013-09-02 21:59:32 +0400

    integration test eligib html css tpl

--- application/views/monoffre/num_eligib_info.php
+++ application/views/monoffre/num_eligib_info.php
@@ -11,18 +11,18 @@
   <p>Debit reception: <?php echo $result["interrogeEligibiliteResult"]["Ligne"]["Debit_de_reception"];?></p>
 </div>
 <div>
-<h3 style='margin-top:0px;'>LES SERVICES MEDIASERV <span class='misc-custom-2'><a href='#'><img class='border-gray' src='images/info_icon.png' title='Plus info' alt='Plus info'></a></span></h3>
+<h3 style='margin-top:0px;'>LES SERVICES MEDIASERV <span class='misc-custom-2'><a href='#'><?php echo image('info_icon.png',NULL,array("title"=>"Plus info", "alt"=>"Plus info","class"=>"border-gray")); ?></a></span></h3>
 <ul class='services-mediaserv'>
-  <li class='telephone not-available'><span><img src='images/tel.png'/>T&Eacute;L&Eacute;PHONE</span> Chez Médiaserv vous disposerez de l’ensemble des options sur la téléphonie.</li>
-  <li class='internet <?php echo ($result["interrogeEligibiliteResult"]["Ligne"]["Eligible_ADSL"]?"available":"not-available");?>'><span><img src='images/net.png'/>INTERNET</span> Chez Médiaserv vous disposerez de l’ensemble des options sur la téléphonie.</li>
-  <li class='television <?php echo ($result["interrogeEligibiliteResult"]["Ligne"]["Eligible_televison"]?"available":"not-available");?>'><span><img src='images/tv.png'/>T&Eacute;L&Eacute;VISION</span> Vous ne pourrez malheureusement pas bénéficier de notre offreTV car votre débit est trop faible.</li>
+  <li class='telephone not-available'><span><?php echo image('tel.png');?>T&Eacute;L&Eacute;PHONE</span> Chez Médiaserv vous disposerez de l’ensemble des options sur la téléphonie.</li>
+  <li class='internet <?php echo ($result["interrogeEligibiliteResult"]["Ligne"]["Eligible_ADSL"]?"available":"not-available");?>'><span><?php echo image('net.png');?>INTERNET</span> Chez Médiaserv vous disposerez de l’ensemble des options sur la téléphonie.</li>
+  <li class='television <?php echo ($result["interrogeEligibiliteResult"]["Ligne"]["Eligible_televison"]?"available":"not-available");?>'><span><?php echo image('tv.png');?>T&Eacute;L&Eacute;VISION</span> Vous ne pourrez malheureusement pas bénéficier de notre offreTV car votre débit est trop faible.</li>
 </ul>
 </div>
 <div>
 <h3>CE QUE NOUS POUVONS AUSSI VOUS PROPOSER</h3>
 <form class='frm-options' action='#' onsubmit='javascript:retrieveForfait();return false'> 
-  <label for="checkbox3"><?php echo form_checkbox($input1); ?> Réduisez votre facture en résiliant votre abonnement car vous êtes en zone dégroupée <span><a href='#'><img class='border-gray' src='images/info_icon.png' title='Plus info' alt='Plus info'></a></span></label>
-  <label for="checkbox3"><?php echo form_checkbox($input2);?> Vous pouvez aussi conserver votre numéro de téléphone <span><a href='#'><img class='border-gray' src='images/info_icon.png' title='Plus info' alt='Plus info'></a></span></label>
+  <label for="checkbox3"><?php echo form_checkbox($input1); ?> Réduisez votre facture en résiliant votre abonnement car vous êtes en zone dégroupée <span><a href='#'><?php echo image('info_icon.png',NULL,array("title"=>"Plus info", "alt"=>"Plus info","class"=>"border-gray")); ?></a></span></label>
+  <label for="checkbox3"><?php echo form_checkbox($input2);?> Vous pouvez aussi conserver votre numéro de téléphone <span><a href='#'><?php echo image('info_icon.png',NULL,array("title"=>"Plus info", "alt"=>"Plus info","class"=>"border-gray")); ?></a></span></label>
   <div class='top-20 six custom-column back-button left'><?php echo anchor('mon_offre/redirectToMonOffre',"PRECEDENT",array("class"=>"precedent"));?></div>
   <div class='top-20 six custom-column text-right'><?php echo form_submit($choix_forfait);?></div>
 </form>
