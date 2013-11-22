f7818c13b66cf7e2fca1eb67af8f53e5a4b79d7d
swanand09 <admin@DEV-REDDY1.mediacall.local>
2013-11-22 15:54:00 +0400

    pictos dans dummy 3

--- application/views/general/options_dummy3.php
+++ application/views/general/options_dummy3.php
@@ -12,16 +12,16 @@ if(isset($bouquetTv)&&!empty($bouquetTv)){
           <strong><?php echo $bouquetTv[0]; ?></strong><span class="right"><?php echo number_format($bouquetTv[1],2,',',' ')."€/mois"; ?></span></strong><br><?php echo $bouquetTv[2]; ?> chaînes
       </li>
       <?php if(!empty($tarifOptionEden)&&isset($tarifOptionEden)){ ?>    
-              <li class="small-icon-heart"><strong><?php echo isset($dummy3[3]["Valeurs"]["Libelle"]["string"])?$dummy3[3]["Valeurs"]["Libelle"]["string"]:""; ?></strong><span class="right" style="margin-left:50px;"><?php echo number_format($tarifOptionEden,2,',',' ')."€/mois"; ?></span><!--<br>(+18)--></li>
+              <li class="eden"><strong><?php echo isset($dummy3[3]["Valeurs"]["Libelle"]["string"])?$dummy3[3]["Valeurs"]["Libelle"]["string"]:""; ?></strong><span class="right" style="margin-left:50px;"><?php echo number_format($tarifOptionEden,2,',',' ')."€/mois"; ?></span><!--<br>(+18)--></li>
       <?php } 
             if(!empty($tarifOptionBein)&&isset($tarifOptionBein)){ ?>    
-              <li class="small-icon-heart"><strong><?php echo isset($dummy3[4]["Valeurs"]["Libelle"]["string"])?$dummy3[4]["Valeurs"]["Libelle"]["string"]:""; ?></strong><span class="right" style="margin-left:50px;"><?php echo number_format($tarifOptionBein,2,',',' ')."€/mois"; ?></span><!--<br>(+18)--></li>
+              <li class="sport"><strong><?php echo isset($dummy3[4]["Valeurs"]["Libelle"]["string"])?$dummy3[4]["Valeurs"]["Libelle"]["string"]:""; ?></strong><span class="right" style="margin-left:50px;"><?php echo number_format($tarifOptionBein,2,',',' ')."€/mois"; ?></span><!--<br>(+18)--></li>
       <?php } 
              if(!empty($bouquetTv[3])&&isset($bouquetTv[3])){ ?> 
-              <li><strong><?php echo isset($dummy3[5]["Valeurs"]["Libelle"]["string"])?utf8_encode($dummy3[5]["Valeurs"]["Libelle"]["string"]):""; ?></strong><span class="right" style="margin-left:50px;"><?php echo ($bouquetTv[3]=="inclus")?$bouquetTv[3]:number_format($bouquetTv[3],2,',',' ')."€/mois"; ?></span><!--<br>(+18)--></li>
+              <li class="vod"><strong><?php echo isset($dummy3[5]["Valeurs"]["Libelle"]["string"])?utf8_encode($dummy3[5]["Valeurs"]["Libelle"]["string"]):""; ?></strong><span class="right" style="margin-left:50px;"><?php echo ($bouquetTv[3]=="inclus")?$bouquetTv[3]:number_format($bouquetTv[3],2,',',' ')."€/mois"; ?></span><!--<br>(+18)--></li>
       <?php }
              if(!empty($bouquetTv[4])&&isset($bouquetTv[4])){?>
-              <li><strong><?php echo isset($dummy3[6]["Valeurs"]["Libelle"]["string"])?utf8_encode($dummy3[6]["Valeurs"]["Libelle"]["string"]):""; ?></strong><span class="right" style="margin-left:50px;"><?php echo ($bouquetTv[4]=="inclus")?$bouquetTv[4]:number_format($bouquetTv[4],2,',',' ')."€/mois"; ?></span><!--<br>(+18)--></li>   
+              <li class="dvr"><strong><?php echo isset($dummy3[6]["Valeurs"]["Libelle"]["string"])?utf8_encode($dummy3[6]["Valeurs"]["Libelle"]["string"]):""; ?></strong><span class="right" style="margin-left:50px;"><?php echo ($bouquetTv[4]=="inclus")?$bouquetTv[4]:number_format($bouquetTv[4],2,',',' ')."€/mois"; ?></span><!--<br>(+18)--></li>   
       <?php } ?>
     </ul>
 </div>
